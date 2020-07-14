d31844017eaeca195791a721dd3ab52da90315a2
wyq <852952709@qq.com>
2020-07-14 23:08:56 +0800

    7-14

--- src/main/java/com/wyq/music/controller/MusicController.java
+++ src/main/java/com/wyq/music/controller/MusicController.java
@@ -4,6 +4,7 @@ import com.wyq.music.entity.Music;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -37,12 +38,19 @@ public class MusicController {
     @RequestMapping("/sing")
     public ModelAndView sing(HttpServletRequest request, HttpServletResponse response){
         ModelAndView modelAndView=new ModelAndView();
-        Music music=new Music(1,"a","a","a",10,"");
-        Music music1=new Music(1,"a","a","a",10,"");
-        List<Music> list=new ArrayList<Music>();
-        modelAndView.addObject("songs",music);
         modelAndView.setViewName("index_sing");
         return modelAndView;
+    }
+    //返回json数据
+    @RequestMapping("/data")
+    @ResponseBody
+    public List<Music> getdata(){
+        List<Music> list=new ArrayList<Music>();
+        for (int i = 0; i <100 ; i++) {
+            Music music=new Music(i,"a"+i,"a","a",10,"");
+            list.add(music);
+        }
+        return list;
     }
 }
 
